71e03ff84bfa16fe755705f0e96bb30c0ee03223
Pawel <skubala.pawel@wp.pl>
2020-10-08 14:29:43 +0200

    updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import pandas as pd
+ # 1. tworzenie wirtualnego środowiska
+ #          python3 -m venv ./venv                   "./venv" -> ścieżka do katalogu wirtualnego środowiska
+ # 2. aktywacja środowiska:
+ #          source ./venv/bin/activate
+ # 3. instalacja "pipenv" do kontroli zależności i automatycznej instalacji bibliotek (pipenv tworzy katalogi Pipfile i Pipfile.lock)
+ #          pip3 install pipenv
+ # 4. Kolejne paczki instalujemy przy pomocy:
+ #          pipenv install <package_name>
 
 
 
