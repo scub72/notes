e9f323e82f3fabd466e43d335d8a6a0e09dd8f70
Pawel <skubala.pawel@wp.pl>
2020-10-09 11:03:27 +0200

    updated main.py with manual entries

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+"""
+======================      Manual      ===========================
+Libraries:
+    :package virtualenvSetting - sets virtualenv and installs pipenv for dependency control
 
+Files:
+    .gitignore - file to exclude unnecessary files
+    Pipfile - file created by pipenv to log the dependencies
+    Pipfile.lock - lockfile for pipenv to install required packages and set working environment
+
+"""
 
 
