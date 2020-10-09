4bbd25db0153125db16645ba9ef25b7ad5c3b20f
Pawel <skubala.pawel@wp.pl>
2020-10-09 09:05:00 +0200

    merged virtualenvSetting to main

--- virtualenvSetting/__init__.py
+++ virtualenvSetting/__init__.py
@@ -1,43 +1,49 @@
 import os
 from sys import platform
 
-def createVirtualEnv():
-    """
-    1. Creating virtual environment
-              python3 -m venv ./venv                   "./venv" -> path to virtual environment catalogue
-    2. Activating virtual environment:
-              source ./venv/bin/activate
-    3. Installing "pipenv" for dependencies control and automatic libraries installation
-        (pipenv creates catalogues Pipfile i Pipfile.lock)
-              pip3 install pipenv
-    4. Next packages we install with:
-              pipenv install <package_name>
-    """
 
-    if platform == "linux" or platform == "linux2":
-        """ Setting linux virtual environment"""
-        try:
-            os.system('python3 -m venv ./venv')
-            print('=> venv catalogue created')
+class VirtualenvSetting:
 
-            os.system('source ./venv/bin/activate')
-            print('=> virtual environment activated')
+    def __init__(self):
+        pass
 
-            os.system('pip3 install pipenv')
-            print('=> pipenv installed')
+    def create_virtualenv(self):
+        """
+        1. Creating virtual environment
+                  python3 -m venv ./venv                   "./venv" -> path to virtual environment catalogue
+        2. Activating virtual environment:
+                  source ./venv/bin/activate
+        3. Installing "pipenv" for dependencies control and automatic libraries installation
+            (pipenv creates catalogues Pipfile i Pipfile.lock)
+                  pip3 install pipenv
+        4. Next packages we install with:
+                  pipenv install <package_name>
+        """
 
-            print('\n\tNow you should install files with "pipenv install <package_name>"'
-                  '\n\tThe virtual environment is set.\n')
-        finally:
-            print('Probably you should install python3-venv.')
+        if platform == "linux" or platform == "linux2":
+            """ Setting linux virtual environment"""
+            try:
+                os.system('python3 -m venv ./venv')
+                print('=> venv catalogue created')
 
-    elif platform == "darwin":
-        """ Setting OSX virtual environment"""
-        pass # here should be osx command
+                os.system('source ./venv/bin/activate')
+                print('=> virtual environment activated')
 
-    elif platform == "win32":
-        """ Setting Windows virtual environment"""
-        pass  # here should be osx command
+                os.system('pip3 install pipenv')
+                print('=> pipenv installed')
 
-    else:
-        print("OS cannot be recognized")
+                print('\n\tNow you should install files with "pipenv install <package_name>"'
+                      '\n\tThe virtual environment is set.\n')
+            finally:
+                print('Probably you should install python3-venv.')
+
+        elif platform == "darwin":
+            """ Setting OSX virtual environment"""
+            pass # here should be osx command
+
+        elif platform == "win32":
+            """ Setting Windows virtual environment"""
+            pass  # here should be osx command
+
+        else:
+            print("OS cannot be recognized")
